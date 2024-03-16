5c8311feb0e8901059fd7036983cf891db64a2b3
LizaZapevalova <160137500+LizaZapevalova@users.noreply.github.com>
2024-03-16 21:19:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,31 +3,14 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
-    int left = 0;
-    int right = size;
-    int iCount = 0;
-    int mid = 0;
-    int ark = -1;
-    while (left <= right) {
-      mid = (left + right) / 2;
-      if (arr[mid] == value) {
-        ark = mid;
-      }
-      if (ark[mid] < value) {
-        left = mid + 1;
-      }
-      else {
-        right = mid - 1;
-      }
+  int a = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value) {
+      a += 1;
     }
-    while (arr[ark] == value) {
-      iCount++;
-      ark++;
-    }
-    if (iCount == 0) {
-      return 0;
-    }
-    else {
-      return iCount;
-    }
+  }
+  if (a > 0)
+    return a;
+  else
+    return 0;
 }
